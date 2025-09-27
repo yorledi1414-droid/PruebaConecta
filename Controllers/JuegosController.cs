ec2eacfe5f6b0baff88c20af781acdedfecfdb86
yorledi1414-droid <yorledi1414@gmail.com>
2025-09-27 17:16:25 -0500

    RF12_Seccion de aprendizaje con Juegos interactivos sin usabilidad en los botones.

---
+++ Controllers/JuegosController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace TuProyecto.Controllers
+{
+    public class JuegosController : Controller
+    {
+        // Página principal de Juegos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Juego: Rompecabezas
+        public ActionResult Rompecabezas()
+        {
+            return View();
+        }
+
+        // Juego: Memoria
+        public ActionResult Memoria()
+        {
+            return View();
+        }
+    }
+}
