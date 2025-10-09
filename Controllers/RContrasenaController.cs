3997ef49adc7bda00ff0cecabfe8622795121ed5
Anderson Herrera <herreraandres123456@gmail.com>
2025-10-09 09:08:43 -0500

    RF5_RContraseña, cambios iniciales

---
+++ Controllers/RContrasenaController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace TuProyectoNamespace.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult RContrasena()
+        {
+            return View();
+        }
+    }
+}
