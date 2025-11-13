916a95e51269bdb928a636d767d1670941d2875b
yorledi1414-droid <yorledi1414@gmail.com>
2025-11-13 01:10:05 -0500

    RF_Vistas de administrador y home

---
+++ Controllers/ReporteController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+using Rotativa;
+
+namespace PruebaConecta.Controllers
+{
+    public class ReporteController : Controller
+    {
+        public ActionResult HistoriaClinica()
+        {
+            return View();
+        }
+
+        public ActionResult GenerarReporte()
+        {
+            var pdf = new ActionAsPdf("HistoriaClinica", new { esPDF = "true" }) 
+            {
+                FileName = $"Reporte_Paciente_{DateTime.Now:dd-MM-yyyy}.pdf"
+            };
+            return pdf;
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Repositories/Model/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PruebaConecta.Repositories.Model
+{
+    public class UserRepository
+    {
+    }
+}
